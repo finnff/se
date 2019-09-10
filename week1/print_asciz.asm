64c50084436dd7ac649dbf2d97ca2d92121afc70
finnff <finn.fonteijn@student.hu.nl>
2019-09-10 15:49:12 +0200

    @s

--- week1/print_asciz.asm
+++ week1/print_asciz.asm
@@ -24,21 +24,21 @@ done:
 
 
 toggle_case:
-  ldrb    r0, [r0]        
-  cmp     r0, #96     
+  ldrb    r0, [r0]   @load r0 registers
+  cmp     r0, #96    @compare with 'a'
   bls     lowercase  @branch lower if char is less than 96('a')
-  cmp     r0, #122
+  cmp     r0, #122   @comapre with 'z'
   bhi     lowercase  @branch lower if char is higher than 122('z')
-  sub     r0, r0, #32
-  b end
+  sub     r0, r0, #32@subract 32 to capitalize
+  b end              @end routine
   
 lowercase:
-  cmp     r0, #64
-  bls     end
-  cmp     r0, #90
-  bhi     end
-  add     r0, r0, #32
-  b end
+  cmp     r0, #64    @compare with"A"
+  bls     end        @branch if less than 64 
+  cmp     r0, #90    @compare with "Z"
+  bhi     end        @branch if higher than 90
+  add     r0, r0, #32@add 32 to lowercase
+  b end              @ end subroutine
 
 end:
-  mov pc, lr
+  mov pc, lr         @move program counter
