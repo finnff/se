37a4e3a33612929fa1199acd5e13be8f10c89a22
finnff <finn.fonteijn@student.hu.nl>
2019-11-01 11:31:36 +0100

    yeet yeet

--- week4/armdecomp.asm
+++ week4/armdecomp.asm
@@ -1,3 +1,5 @@
+//@finnff
+
 .cpu cortex-m0 
 .bss
 	buffer: .fill 0x28 //fill with 40
@@ -5,66 +7,67 @@
 .align 2 
 .global armdecomp
 
+
 armdecomp:
-	PUSH {R4, R5, R6, R7, LR} 	//push alle registers naar de stack
-	LDR	R3, =buffer				//start van buffer in R3
-	LDR	R4, =encoded			//in R4 word een pointer naar de eerste geheugenlocatie geplaatst
-	MOV	R5, #0					//init index pointer met 0	
-	MOV	R6, #0					//init offset met 0
-	MOV	R7, #0					//init lengte met 0
+	PUSH {R4, R5, R6, R7, LR} //Push R4-R7+PC to stack
+	LDR	R3, =buffer						// Load Buffer in R3
+	LDR	R4, =encoded					// Load Encoded in R4
+	MOV	R5, #0								// generate index
+	MOV	R6, #0								// generate offset
+	MOV	R7, #0								// generate length
 	
+
 loop1:
-	ADD R0, R4, R5				//stop het geheugenadress van R4 met index R5 in R0
-	LDRB R0, [r0]				//haal de waarde van het geheugenadress op en stop het in R0
-	ADD R5, R5, #1				//verhoog index pointer met 1
-	CMP R0, #0					//controleer of we het einde van compressed bereikt hebben
-	BEQ	done
+	ADD R0, R4, R5			//move pointer R4[R5] into R0
+	LDRB R0, [r0]				//Dereference value from R0
+	ADD R5, R5, #1			//increment pointer
+	CMP R0, #0					//set flags for #0
+	BEQ	done						// if R0 ==0, branch to done
 	
-	CMP R0, #'@'				//controleer of het character in R0 compressed is
-	BEQ	decode				//in geval van compressie branch naar decode
-	bl swap					//branch link naar de swap functie
-	bl uart_put_char			//print het character in R0
-	b loop1					//ga verder met door compressed itereren
-	
+	CMP R0, #'@'				//Set Flags for #@
+	BEQ	decode					// If R0 == @, Branch to decode
+	bl swap					    // branch link to Swap
+	bl uart_put_char		// branch link to Print
+	b loop1							// restart loop1
 	
 	
 decode:
-	ADD R6, R4, R5				//stop het geheugenadress van R4 met index R5 in R6
-	LDRB r6, [R6]				//haal de waarde van het geheugenadress op en stop het in R6
-	ADD R5, R5, #1				//verhoog de index pointer met 1
-	ADD R7, R4, R5				//stop het geheugenadress van R4 met index R5 in R7
-	LDRB r7, [R7]				//haal de waarde van het geheugenadress op en stop het in R7
-	ADD R5, R5, #1				//verhoog de index pointer met 1
-	SUB R6, R6, #'0'			//trek er '0'(==48) van af
-	SUB R7, R7, #'0'			//trek er '0'(==48) van af
+	ADD R6, R4, R5				//move pointer R4[R5] into R6	
+	LDRB r6, [R6]					//Dereference pointer into R6	
+	ADD R5, R5, #1				//Increment R5								
+	ADD R7, R4, R5				//move pointer R4[R5] into R7	
+	LDRB r7, [R7]					//Dereference pointer into R6	
+	ADD R5, R5, #1				//increment R5								
+	SUB R6, R6, #48			  //Subtract 48 from R6		
+	SUB R7, R7, #48			  //Subtract 48 from R7		
 	
 decodeLoop:
-	ADD R0, R3, R6				//tell R6 bij R3 op en stop dat in R0
-	LDRB R0, [R0]				//haal de waarde van het geheugenadress op en stop het in R0
-	bl swap					//branch link naar de swap functie
-	bl uart_put_char			//print het character in R0
-	SUB	R7, R7, #1				//verlaag lengte met 1
-	BEQ loop1				//als het hele woord gedecodeed is ga door met loop1
-	b decodeLoop			//ga verder met het woord decodeen
+	ADD R0, R3, R6				// Add R3+R6 into R0											
+	LDRB R0, [R0]				 	// dereference into R0										
+	bl swap								// Branchlink swap												
+	bl uart_put_char			// Branchlink Print												
+	SUB	R7, R7, #1				// Subtract 1 from R7											
+	BEQ loop1			      	// Brance to Loop1, once word is finishe`d	
+	b decodeLoop			   	// Else continue looping									
 	
 swap:
 	PUSH {LR}
-	MOV R1, #39					//zet de buffer lengte in R1
-swapLoop:
-	SUB R1, R1, #1 				//zet R1 op de vorige index door er 1 vanaf te halen
-	ADD R2, R3, R1 				//stop het geheugenadress van R3 met index R1 in R2
-	LDRB R2, [R2] 				//Haal de waarde van het geheungenadress op en zet dat in R2
-	ADD R1, R1, #1				//tel 1 op bij R1
-	STRB R2, [R3, R1]			//store het character op buffer[R1]
-	SUB R1, R1, #1				//trek 1 van R1 af voor de volgende loop
+	MOV R1, #39						//Set Buffer length in R1
 
-	CMP R1, #0					//compare R1 met 0
-	BEQ SwapEnd				//als het gelijk is branch naar SwapEnd
-	b swapLoop				//zo niet ga door met swapLoop
+swapLoop:
+	SUB R1, R1, #1 				// Subtract 1 From R1 to return to previous index		
+	ADD R2, R3, R1 				//move pointer R3[R1] into R2												
+	LDRB R2, [R2] 				//Dereference Pointer into R2												
+	ADD R1, R1, #1				//Increment R1																			
+	STRB R2, [R3, R1]			//Store R2 on buffer ar R3[R1]											
+	SUB R1, R1, #1				//Remove 1 from R1																	
+	CMP R1, #0						//Set Flags																					
+	BEQ SwapEnd						//If R1==0, Branch to SwapEnd												
+	b swapLoop						//Else Continue Looping															
 	
 SwapEnd:
-	STRB R0, [R3, #0]			//plaats huidige char op index 0
+	STRB R0, [R3, #0]			//Store R0 onto R3[0]
 	POP {PC}	
 	
 done:
-	POP {R4, R5, R6, R7, PC} 	//pop alles weer van de stack
+	POP {R4, R5, R6, R7, PC} 	//pop R4-R7+PC From stack
